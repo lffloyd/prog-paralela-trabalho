dc0a9101fd77b78568d57dc3178f0751351d0516
Luiz Matos <luiz.matos@amedigital.com>
2020-11-02 18:23:24 -0300

    feat: cria utils para testes

---
+++ src/utils/test_utils.h
@@ -0,0 +1,9 @@
+#ifndef TEST_UTILS_H_
+#define TEST_UTILS_H_
+
+#define TEST_MTX_NROWS 5
+#define TEST_MTX_NCOLS 5
+
+int **create_dynamic_matrix_from_static(int static_mtx[TEST_MTX_NROWS][TEST_MTX_NCOLS]);
+
+#endif
